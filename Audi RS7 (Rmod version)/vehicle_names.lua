4cbb95bb9d5b870cd131ec1ff6bbe78f0c3e92e3
istoking <58592678+istoking@users.noreply.github.com>
2020-02-20 01:27:36 +1300

    Add files via upload

---
+++ Audi RS7 (Rmod version)/vehicle_names.lua
@@ -0,0 +1,7 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	AddTextEntry('rmodrs7', 'Audi RS7 Sportback, Custom')
+end)
