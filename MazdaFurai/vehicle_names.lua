e17fee1cee6df5a9088da05b61cb77a22eb0e700
istoking <58592678+istoking@users.noreply.github.com>
2020-02-20 02:20:22 +1300

    Add files via upload

---
+++ MazdaFurai/vehicle_names.lua
@@ -0,0 +1,7 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	AddTextEntry('furaiub', 'Mazda Furai (Concept Car)')
+end)
