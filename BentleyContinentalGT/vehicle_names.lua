503ba189d6b21abed2d7925d3e9012ef95152955
istoking <58592678+istoking@users.noreply.github.com>
2020-03-06 00:56:25 +1300

    Add files via upload

---
+++ BentleyContinentalGT/vehicle_names.lua
@@ -0,0 +1,7 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	AddTextEntry('contss18', '2018 Bentley Continental GT Supersports')
+end)
