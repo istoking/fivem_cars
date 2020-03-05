b6f4f6d7f6fb3498c9faa46d5b66c872f8dde47f
istoking <58592678+istoking@users.noreply.github.com>
2020-03-06 01:22:46 +1300

    Add files via upload

---
+++ Porsche911GT3R/vehicle_names.lua
@@ -0,0 +1,7 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	AddTextEntry('911gt3r', 'Porsche 911 GTR 3')
+end)
